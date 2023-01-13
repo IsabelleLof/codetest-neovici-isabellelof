49e154d88244880bfb4123d8b901079598a5bc7b
IsabelleLof <104595550+IsabelleLof@users.noreply.github.com>
2023-01-13 17:03:54 +0100

    created codetest project for neovici

---
+++ src/FullName.js
@@ -0,0 +1,56 @@
+import {
+  html,
+  component,
+  useState,
+  useEffect
+} from "https://unpkg.com/haunted/haunted.js";
+
+function FullName(el) {
+  const [first, setFirst] = useState("Happy");
+  const [last, setLast] = useState("Halloween 🎃");
+
+  useEffect(() => {
+    const event = new CustomEvent('change', {
+      detail: `${first} ${last}`
+    });
+    this.dispatchEvent(event);
+  }, [first, last]);
+
+  return html`
+    <div class="container">
+      <label for="first-name">First</label>
+      <input
+        value=${first}
+        @keyup=${ev => setFirst(ev.target.value)}
+        type="text"
+        name="first"
+        id="first-name"
+      />
+
+      <label for="last-name">Last</label>
+      <input
+        value=${last}
+        @keyup=${ev => setLast(ev.target.value)}
+        type="text"
+        name="last"
+        id="last-name"
+      />
+    </div>
+
+    <style>
+      .container {
+        border: none;
+        display: grid;
+        grid-template-columns: 20% 80%;
+      }
+
+      input {
+        border: 1px solid #e5e5e5;
+        padding: 6px 10px;
+        margin-bottom: 1em;
+      }
+    </style>
+  `;
+}
+
+customElements.define("full-name", component(FullName));
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import {
+  html,
+  component,
+  useState,
+} from "https://unpkg.com/haunted/haunted.js";
+import "./FullName.js";
+
+function App() {
+  const [name, setName] = useState("");
+
+  return html`
+    <h2>User Page</h2>
+
+    <h3>${name}</h3>
+
+    <fieldset>
+      <legend>Change name:</legend>
+      <full-name @change=${ev => setName(ev.detail)}></full-name>
+    </fieldset>
+
+    <style>
+      fieldset {
+        border: none;
+      }
+
+      legend {
+        padding: 0;
+        margin-bottom: 0.5rem;
+      }
+    </style>
+  `;
+}
+
+customElements.define("my-app", component(App));
